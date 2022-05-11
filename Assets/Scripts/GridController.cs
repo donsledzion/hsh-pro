eac53e41d16ee35866d2ef8332d84612e1810da0
DESKTOP-K94QVJ7\choja <adam.chojaczyk@epicvr.pl>
2022-05-12 00:16:05 +0200

    Grid starts working :)

---
+++ Assets/Scripts/GridController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GridController : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI gridLabelText;
+    [SerializeField] Slider gridLabelSlider;
+    [SerializeField] TextMeshProUGUI gridToggleButtonText;
+
+
+
+    [SerializeField] GameObject whiteboard;
+    [SerializeField] GameObject gridDotPrefab;
+    [SerializeField] Transform dotsContainer;
+
+    private bool _gridIsOn;
+    public void UpdateLabel()
+    {
+        gridLabelText.text = "Grid density: " + gridLabelSlider.value + " cm";
+        if (_gridIsOn)
+            GenerateGrid();
+    }
+
+    private void GenerateGrid()
+    {
+        _gridIsOn = true;
+        ClearGrid();
+        int gridOffset = (int)gridLabelSlider.value;
+        Vector2 gridSize = new Vector2(whiteboard.GetComponent<RectTransform>().rect.width, whiteboard.GetComponent<RectTransform>().rect.width);
+        Debug.Log("Generating grid: grid size: " + gridSize.x + ", " + gridSize.y + " | gridOffset: " + gridOffset );
+        for(int i  = gridOffset; i < gridSize.y; i+=gridOffset)
+            for (int j = gridOffset; j < gridSize.x; j += gridOffset)
+            {
+                GameObject dot = Instantiate(gridDotPrefab, GameManager.ins.drawingCanvasBackgroundLBCorner+new Vector3(j * GameManager.ins.zoom, i * GameManager.ins.zoom, 0),gridDotPrefab.transform.rotation);
+                dot.transform.SetParent(dotsContainer);
+            }
+    }
+
+    private void ClearGrid()
+    {
+        foreach(Transform dot in dotsContainer.GetComponentsInChildren<Transform>())
+        {
+            if(dot!=dotsContainer)
+                Destroy(dot.gameObject);
+        }
+    }
+
+    public void ToggleGrid()
+    {
+        if(_gridIsOn)
+        {
+            _gridIsOn = false;
+            ClearGrid();
+            gridToggleButtonText.text = "Grid: OFF";
+        }
+        else
+        {
+            _gridIsOn = true;
+            GenerateGrid();
+            gridToggleButtonText.text = "Grid: ON";
+        }
+    }
+}
