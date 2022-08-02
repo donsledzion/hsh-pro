f13d95516e5227cb29b247534cf4f89d44797f83
DESKTOP-K94QVJ7\choja <adam.chojaczyk@epicvr.pl>
2022-08-02 21:23:52 +0200

    Changes to storey display info panel

--- Assets/StoreyListButton.cs
+++ Assets/StoreyListButton.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class StoreyListButton : MonoBehaviour
 {
-    Storey2D _storey2D;
+    public Storey2D Storey2D { get; private set; }
     public bool IsChosen { get; set; }
+    [SerializeField] TextMeshProUGUI buttonLabel;
+
+    public void Initialize(Storey2D storey2D)
+    {
+        buttonLabel.text = storey2D.StoreyReference.Name;
+        Storey2D = storey2D;
+        IsChosen = false;
+    }
 }
--- Assets/StoreyListSwitcher.cs
+++ Assets/StoreyListSwitcher.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class StoreyListSwitcher : MonoBehaviour
 {
     List<StoreyListButton> _storeyListButtons = new List<StoreyListButton>();
+    [SerializeField] GameObject buttonPrefab;
+    [SerializeField] Transform buttonsContainer;
 
 
     [ContextMenu("Update List")]
@@ -13,7 +15,9 @@ public class StoreyListSwitcher : MonoBehaviour
         List<Storey2D> storeys2D = Drawing2DController.ins.Storeys2D;
         foreach(Storey2D storey2D in storeys2D)
         {
-
+            GameObject button = Instantiate(buttonPrefab, buttonsContainer);
+            StoreyListButton listButton = button.GetComponent<StoreyListButton>();
+            listButton.Initialize(storey2D);
         }
     }
 }
