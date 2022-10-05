c3e6a9b7b9a465836cd5d5c3b2cac7c2bc61f02c
wertalus <wertalus@gmail.com>
2022-10-05 20:27:10 +0200

    Header tabs controller added

--- Assets/Scripts/UI/ModeController.cs
+++ Assets/Scripts/UI/ModeController.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityTemplateProjects;
+
 
 public class ModeController : MonoBehaviour
 {
@@ -22,21 +25,116 @@ public class ModeController : MonoBehaviour
 
     public List<GameObject> listOf2DObjects = new List<GameObject>();
     public List<GameObject> listOf3DObjects = new List<GameObject>();
-    public SimpleCameraController cameraController;
+    public List<GameObject> listOfMenuObjects = new List<GameObject>();
 
-    public void Mode2D() { 
-    
+    public SimpleCameraController _cameraController;
+    public GameObject _builder3D;
+    Builder3D refToScript;
+
+    public void Mode2D() {
+
+        ResetAllListObjects();
         foreach(GameObject objects2D in listOf2DObjects)
         {
             objects2D.SetActive(true);
-            if (objects2D.name == "Main Camera") {
 
-                cameraController = objects2D.GetComponent<SimpleCameraController>();
-                //cameraController.isActiveAndEnabled(false);
+            _cameraController.enabled = false;
+
+            switch (objects2D.name)
+            {
+                case "Main Body Layout":
+
+                    objects2D.GetComponent<Image>().enabled = true;
+
+                break;
+
+                case "Left Side":
+
+                    objects2D.GetComponent<Image>().enabled = true;
+
+                break;
+
+                default:
+                    break;
             }
         }
-    
     }
 
+    public void Mode3D()
+    {
+        ResetAllListObjects();
 
+        _cameraController.enabled = true;
+        refToScript = _builder3D.GetComponent<Builder3D>();
+        refToScript.GenerateBuilding();
+        
+        
+
+        foreach (GameObject objects3D in listOf3DObjects)
+        {
+            objects3D.SetActive(true);
+ 
+        }
+            
+    }
+
+    public void ModeMenu() {
+
+        foreach (GameObject objectsMenu in listOfMenuObjects)
+        {
+            objectsMenu.SetActive(true);
+
+            _cameraController.enabled = false;
+
+            switch (objectsMenu.name)
+            {
+                case "Main Body Layout":
+
+                    objectsMenu.GetComponent<Image>().enabled = true;
+
+                    break;
+
+                case "Left Side":
+
+                    objectsMenu.GetComponent<Image>().enabled = true;
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void ResetAllListObjects()
+    {
+        foreach (GameObject objects2D in listOf2DObjects)
+        {
+            objects2D.SetActive(false);
+            switch (objects2D.name)
+            {
+
+                case "Main Body Layout":
+
+                    objects2D.GetComponent<Image>().enabled = false;
+
+                    break;
+
+                case "Left Side":
+
+                    objects2D.GetComponent<Image>().enabled = false;
+
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+
+        foreach (GameObject objects3D in listOf3DObjects)
+        {
+            objects3D.SetActive(false);
+        }
+    }
 }
