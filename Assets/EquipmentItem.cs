c4565c2a22c3f33334f96b7b86bccfc24d3e1bfe
DESKTOP-K94QVJ7\choja <adam.chojaczyk@epicvr.pl>
2022-10-24 21:33:53 +0200

    Furniture prefabs updated with thumb scale

---
+++ Assets/EquipmentItem.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentItem : MonoBehaviour
+{
+    [SerializeField] Vector3 _thumbScale = new Vector3(1f, 1f, 1f);
+}
