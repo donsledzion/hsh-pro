7171ab981ce1a65b417ce38d01c9a1f6c73e8fb4
wertalus <wertalus@gmail.com>
2022-11-13 16:36:48 +0100

    3D Objects added

--- Assets/ToggleOnOff.cs
+++ Assets/ToggleOnOff.cs
@@ -1,18 +1,87 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+using UnityEngine.Events;
 
-public class ToogleOnOff : MonoBehaviour
+public class ToggleOnOff : MonoBehaviour, IPointerDownHandler
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public delegate void ValueChanged(bool value);
+    public event ValueChanged valueChanged;
+
+    [SerializeField]
+    UnityEvent onToggle;
+
+    [SerializeField]
+    GameObject equipmentSelection;
+
+    [SerializeField]
+    private Image backgroundImage;
+
+    [SerializeField]
+    private bool _isOn = false;
+
+    private bool _gameObjectIsOn;
+
+    public bool isOn
     {
-        
+        get
+        {
+            return _isOn;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerDown(PointerEventData eventData)
     {
-        
+        Toggle(!isOn);
     }
+
+    private void Toggle(bool value)
+    {
+        if (value != isOn)
+        {
+            _isOn = value;
+
+            ToggleColor(isOn);
+
+            //ToggleObject(isOn);
+
+        }
+
+        if (valueChanged != null)
+        {
+            valueChanged(isOn);
+        }
+
+        onToggle?.Invoke();
+    }
+    private void ToggleColor(bool value)
+    {
+        if (value)
+            backgroundImage.enabled = true;
+        else
+            backgroundImage.enabled = false;
+    }
+
+    public void ToggleObject()
+    {
+        if (_gameObjectIsOn)
+        {
+            _gameObjectIsOn = false;
+        }
+        else
+        {
+            _gameObjectIsOn = true;
+        }
+    }
+
+    private void ToggleObject(bool value)
+    {
+        if (value)
+            equipmentSelection.SetActive(true);
+        else
+            equipmentSelection.SetActive(false);
+    }
+
 }
