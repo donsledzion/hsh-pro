c092f8091e054614b12bf324407b6d2e175beef7
DESKTOP-K94QVJ7\choja <adam.chojaczyk@epicvr.pl>
2022-11-15 22:16:44 +0100

    Rebuild bundles and created one direction snap system

--- Assets/WallEquipmentItem.cs
+++ Assets/WallEquipmentItem.cs
@@ -1,43 +1,114 @@
 using System.Collections;
 using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditorInternal;
+#endif
 using UnityEngine;
 
 public class WallEquipmentItem : EquipmentItem
 {
     [SerializeField] protected LayerMask _wallLayer;
-
+    [SerializeField] protected float _snapDistance = 30;
     bool _isTouchingSnapSurface = false;
 
     public bool IsTouchingSnapSurface { get { return _isTouchingSnapSurface; } }
 
-    [SerializeField] Collider _snapCollider;
+    Plane _snapPlane;
 
-    public Collider SnapCollider { get { return _snapCollider; } }   
+    GameObject _duplicate;
 
-    public LayerMask WallLayer {  get { return _wallLayer; } }
+    public GameObject Duplicate { get { return _duplicate; } }
 
+    public Plane SnapPlane { get { return _snapPlane; } }
 
-    protected override void OnTriggerStay(Collider other)
+    Collider _hitCollider;
+
+    public bool isDuplicate = false;
+
+    public new bool IsColliding
     {
-        base.OnTriggerStay(other);
-
-        if (_wallLayer == (_wallLayer | (1 << other.gameObject.layer)))
+        get
         {
-            Debug.Log("Snap collision!");
-            _isTouchingSnapSurface = true;
-            _snapCollider = other;
+            if (_duplicate == null)
+                return _isColliding;
+            return _duplicate.GetComponent<WallEquipmentItem>()._isColliding;
         }
-
-            
     }
-    protected override void OnTriggerExit(Collider other)
-    {
-        base.OnTriggerExit(other);
 
-        if (_wallLayer == (_wallLayer | (1 << other.gameObject.layer)))
+    public Transform MountTransform
+    {
+        get
         {
-            _isTouchingSnapSurface = false;
-            _snapCollider = null;
+            if (_duplicate == null)
+                return transform;
+            return _duplicate.transform;
         }
+    }
+
+    public LayerMask WallLayer { get { return _wallLayer; } }
+
+
+    private void Update()
+    {
+        if (isDuplicate) return;
+        Ray ray = new Ray(transform.position,transform.forward);
+        RaycastHit hit;        
+        if (Physics.Raycast(ray, out hit, 200f, WallLayer))
+        {
+#if UNITY_EDITOR
+            Debug.DrawRay(transform.position, ray.direction * _snapDistance, Color.green);
+#endif
+            if (_hitCollider != hit.collider)
+                GetRidOffDuplicate();
+
+            _hitCollider = hit.collider;
+            if (_hitCollider.gameObject.CompareTag("WallSnapSurface"))
+            {
+                _snapPlane = _hitCollider.GetComponent<WallPlane>().Plane;
+
+                if (_duplicate == null)
+                {
+                    
+                    transform.eulerAngles = new Vector3(transform.eulerAngles.z, hit.collider.transform.eulerAngles.y - (_snapPlane.GetSide(transform.position) ? 180 : 0), transform.eulerAngles.z);
+                    _duplicate = Instantiate(this.gameObject, this.transform.parent);
+                    _duplicate.GetComponent<WallEquipmentItem>().MarkAsDuplicate();
+                    _duplicate.GetComponent<WallEquipmentItem>().enabled = false;
+                    RenderMe(false);
+                }
+                _duplicate.transform.position = hit.point;
+            }
+        }
+        else
+        {
+#if UNITY_EDITOR
+            Debug.DrawRay(transform.position, ray.direction * _snapDistance, Color.red);
+#endif
+            GetRidOffDuplicate();
+        }
+    }
+
+    void GetRidOffDuplicate()
+    {
+        RenderMe(true);
+        Destroy(_duplicate);
+        _duplicate = null;
+    }
+
+    private void RenderMe(bool state)
+    {
+        foreach(MeshRenderer renderer in transform.GetComponentsInChildren<MeshRenderer>())
+        {
+            renderer.enabled = state;
+        }
+    }
+
+    public void MarkAsDuplicate()
+    {
+        isDuplicate = true;
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(Duplicate);
     }
 }
