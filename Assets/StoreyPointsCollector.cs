9945c4bf4e8cac8058118d538d572c7582ef2ea5
DESKTOP-K94QVJ7\choja <adam.chojaczyk@epicvr.pl>
2022-11-16 20:47:10 +0100

    Added storey points collector

---
+++ Assets/StoreyPointsCollector.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreyPointsCollector : MonoBehaviour
+{
+    Vector2[] _storeyPoints;
+
+    internal class WallPoint
+    {
+        public Vector2 point;
+        public WallSection2D wallSection;
+    }
+
+    public Vector2[] CollectAllPoints()
+    {
+        Vector2[] vector2s = new Vector2[2];
+
+        return vector2s;
+    }
+}
