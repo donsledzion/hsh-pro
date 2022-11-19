0c8d862218baaa19e8c0ca87c9b137f126a684ae
wertalus <wertalus@gmail.com>
2022-11-19 18:33:19 +0100

    Dining room furniture added.

--- Assets/Scripts/PolygonHelper.cs
+++ Assets/Scripts/PolygonHelper.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Build.Pipeline;
+//using UnityEditor.Build.Pipeline;
 using UnityEngine;
 
 public static class PolygonHelper
