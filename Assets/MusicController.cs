c6f1822d56d7f8d613f8f16c4d03eaf648f94b69
DESKTOP-K94QVJ7\choja <adam.chojaczyk@epicvr.pl>
2022-11-21 20:36:17 +0100

    Added audios controller to hold songs for interactable items

--- Assets/MusicController.cs
+++ Assets/MusicController.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class MusicController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static MusicController ins { get; private set; }
+
+    List<VideoClip> _audioClips = new List<VideoClip>();
+
+    public List<VideoClip> VideoClips { get { return _audioClips; } }
+
+    int _currentSong = 0;
+
+    public VideoClip NextSong()
     {
-        
+        if (_audioClips.Count < 1) return null;
+        _currentSong++;
+        if (_currentSong > _audioClips.Count - 1) _currentSong = 0;
+        return _audioClips[_currentSong];
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void Awake()
     {
-        
+        if (ins != null && ins != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            ins = this;
+        }
     }
 }
