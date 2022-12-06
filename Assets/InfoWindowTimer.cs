83cced4c4df30770cefbf32c110df2812facc8e6
wertalus <wertalus@gmail.com>
2022-12-06 21:29:00 +0100

    Scene update

--- Assets/InfoWindowTimer.cs
+++ Assets/InfoWindowTimer.cs
@@ -10,12 +10,20 @@ public class InfoWindowTimer : MonoBehaviour,IPointerClickHandler
     public string textToDisplay;
     public GameObject infoWindow;
     public GameObject buttonRef;
+    public Vector2 position;
+
+    
 
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        Resolution resolution = Screen.currentResolution;
+        position = new Vector2(resolution.width / 2, position.y);
         infoWindow.SetActive(true);
-        InformationWindowController._instance.ShowToolTipTimer(header, textToDisplay);   
+
+        
+
+        InformationWindowController._instance.ShowToolTipTimer(header, textToDisplay, position);   
     }
 
 /*    public void OnPointerDown(PointerEventData eventData)
