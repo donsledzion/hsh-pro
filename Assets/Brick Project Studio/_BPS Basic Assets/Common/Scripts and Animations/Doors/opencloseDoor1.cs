77ad26bf66ac4d9b61ab1cc06bc05db2307a9c64
DESKTOP-K94QVJ7\choja <adam.chojaczyk@epicvr.pl>
2022-12-08 22:07:04 +0100

    Addes some animations and interactions to several prefabs

--- Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor1.cs
+++ Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor1.cs
@@ -10,48 +10,35 @@ namespace SojaExiles
 
 		public Animator openandclose1;
 		public bool open;
-		public Transform Player;
 
 		void Start()
 		{
 			open = false;
-		}
+        }
 
-		void OnMouseOver()
-		{
-			{
-				if (Player)
-				{
-					float dist = Vector3.Distance(Player.position, transform.position);
-					if (dist < 15)
-					{
-						if (open == false)
-						{
-							if (Input.GetMouseButtonDown(0))
-							{
-								StartCoroutine(opening());
-							}
-						}
-						else
-						{
-							if (open == true)
-							{
-								if (Input.GetMouseButtonDown(0))
-								{
-									StartCoroutine(closing());
-								}
-							}
 
-						}
 
-					}
-				}
+        [ContextMenu("Toggle")]
+        public void Toggle()
+        {
+            if (open)
+                Close();
+            else
+                Open();
+        }
+        [ContextMenu("Open")]
+        public void Open()
+        {
+            StartCoroutine(opening());
+        }
 
-			}
+        [ContextMenu("Close")]
+        public void Close()
+        {
+            StartCoroutine(closing());
+        }
 
-		}
-
-		IEnumerator opening()
+        IEnumerator opening()
 		{
 			print("you are opening the door");
 			openandclose1.Play("Opening 1");
