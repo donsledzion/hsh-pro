3285faef643c4c9ad5b17d6c409facbd93931537
DESKTOP-K94QVJ7\choja <adam.chojaczyk@epicvr.pl>
2022-12-23 17:21:00 +0100

    FloorPlane script update

--- Assets/Scripts/FloorPlane.cs
+++ Assets/Scripts/FloorPlane.cs
@@ -119,9 +119,6 @@ public class FloorPlane : MonoBehaviour
                 ScriptableObjectsController item = bundle.LoadAsset(Floor.MaterialName) as ScriptableObjectsController;
                 Material material = item.material;
                 _renderer.material = material;
-                //_renderer.material.renderQueue = Floor.Order;
-                string tagValue = _renderer.material.GetTag("Queue", true, "Tag not found");
-                Debug.Log("<color=green>Looking for tag PRIORITY: " + tagValue + "</color>");
                 SetTilling(new Vector2(item.tiling_x,item.tiling_y));
             }
             else
